db2113e03168a94b13530be7eb214f91ee095e6e
Kenneth Murerwa <xwaxes@gmail.com>
2019-05-16 15:16:33 +0300

    Update ESP32DevkitControllCode.ino

--- ESP32DevkitControllCode.ino
+++ ESP32DevkitControllCode.ino
@@ -7,9 +7,6 @@
 #define BLYNK_PRINT Serial
 
 //Import libraries
-#include "esp_bt_main.h" //Import bluetooth host stack
-#include "esp_gap_bt_api.h" //import functionalities
-#include "esp_bt_device.h" //Import function for renaming the bluetooth device name
 #include <WiFi.h> //import wifi package
 #include <WiFiClient.h>
 #include <BlynkSimpleEsp32.h>
@@ -19,31 +16,10 @@ const char* ssid = "your router name";
 const char* password = "its password";
 char auth[] = "your blynk auth code";
 
-bool initBluetooth(const char *deviceName) {
-  /*This method starts bluetooth, renames it and returns if
-  **the operation was successful
-  */
-  if (!btStart()) {
-    return false;//Return false if bluetooth could not start
-  }
-  if (esp_bluedroid_init()!= ESP_OK) {
-    return false;//Return false if bluedroid could not be initialized
-  }
-   
-  if (esp_bluedroid_enable()!= ESP_OK) {
-      return false;//Return false if bluedroid could not be enabled
-  }
-  esp_bt_dev_set_device_name(deviceName);//Change device name
-  esp_bt_gap_set_scan_mode(ESP_BT_SCAN_MODE_CONNECTABLE_DISCOVERABLE); //Make bt discoverable
-}
-
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   Blynk.begin(auth, ssid, password);
-  if(!initBluetooth("Bluetooth Name")){//Call the method initBluetooth and attempt to rename the device
-    Serial.println("Bluetooth init failed");
-  };
 }
 
 void loop() {
